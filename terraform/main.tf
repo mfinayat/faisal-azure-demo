97a767b24f703cad4b5d9b90efdeec6b89c16e3a
faisalinayat <msds20042@itu.edu.pk>
2025-12-29 01:50:25 +0100

    Upgrade App Service Plan to B1 to prevent crashes

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 
 # 4. Create the Web App
